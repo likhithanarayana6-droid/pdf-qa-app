34f9e8fa9192bb3623275c8c9f6038f3790b24e5
swethareddyrangamreddy9-stack <swethareddyrangamreddy9@gmail.com>
2026-02-20 21:40:15 +0530

    Initial commit - PDF Q&A app

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("consolelog")
+const cb=document.getElementById("changebtn");                                                                                                                                                  
+console.log(changebtn); 
+function add(){
+console.log("hellooooo sum");
+}
+//add();
+cb.addEventListener('click',add);       
